ea4770337d36b96bc6ae6e4d9d4148aaaf892a5c
Bekithemba-Mdluli <putthetrust.gamedev@gmail.com>
2022-08-18 14:21:55 +0200

    Updated gitignore

--- Tweeter.Web/wwwroot/js/site.js
+++ Tweeter.Web/wwwroot/js/site.js
@@ -3,3 +3,15 @@
 
 // Write your JavaScript code.
 
+function scrollNav() {
+    $('.nav a').click(function () {
+        $(".active").removeClass("active");
+        $(this).addClass("active");
+
+        $('html, body').stop().animate({
+            scrollTop: $($(this).attr('href')).offset().top - 160
+        }, 300);
+        return false;
+    });
+}
+scrollNav();
